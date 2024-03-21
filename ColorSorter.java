ab69cbb67a10a64d55e192ea6fdc5f5753e6258f
Tayib Suhail Sheikh <156133220+tayibs1@users.noreply.github.com>
2024-03-21 01:56:54 +0000

    added some small changes

--- ColorSorter.java
+++ ColorSorter.java
@@ -1,45 +1,165 @@
-private static void sortColor(int colorId) {
-    String colorName = getColorName(colorId); // Convert colorId to colorName
-    LCD.clear();
-    LCD.drawString("Sorting Color: " + colorName, 0, 0);
-    switch (colorId) {
-        case Color.RED:
-            moveToColorPosition(COLOR_POSITIONS[0]);
-            break;
-        case Color.GREEN:
-            moveToColorPosition(COLOR_POSITIONS[1]);
-            break;
-        case Color.BLUE:
-            moveToColorPosition(COLOR_POSITIONS[2]);
-            break;
-        case Color.YELLOW:
-            moveToColorPosition(COLOR_POSITIONS[3]);
-            break;
-        default:
-            LCD.drawString("Unknown Color", 0, 1);
-            break;
-    }
-}
+package pack;
 
-private static String getColorName(int colorId) {
-    switch (colorId) {
-        case Color.NONE:
-            return "NONE";
-        case Color.BLACK:
-            return "BLACK";
-        case Color.BLUE:
-            return "BLUE";
-        case Color.GREEN:
-            return "GREEN";
-        case Color.YELLOW:
-            return "YELLOW";
-        case Color.RED:
-            return "RED";
-        case Color.WHITE:
-            return "WHITE";
-        case Color.BROWN:
-            return "BROWN";
-        default:
-            return "UNKNOWN";
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.Color;
+import lejos.utility.Delay;
+
+public class ABC {
+    private static EV3LargeRegulatedMotor beltMotor = new EV3LargeRegulatedMotor(MotorPort.D);
+    private static EV3LargeRegulatedMotor feedMotor = new EV3LargeRegulatedMotor(MotorPort.A);
+    private static EV3TouchSensor touchSensor = new EV3TouchSensor(LocalEV3.get().getPort("S1"));
+    private static EV3ColorSensor colorSensor = new EV3ColorSensor(LocalEV3.get().getPort("S3"));
+
+    private static final int MAX_OBJECTS = 4;
+    private static final int[] COLOR_POSITIONS = {0, 150, 330, 500}; // red, green, blue, yellow
+
+
+    public static void main(String[] args) {
+        while (true) {
+            // Initialise
+            resetMotors();
+            int[] colorList = new int[MAX_OBJECTS];
+            int colorCount = 0;
+
+            // Scanning loop
+            while (colorCount < MAX_OBJECTS && !Button.ENTER.isDown()) {
+                if (Button.ESCAPE.isDown()) {
+                    return; // Exit program immediately
+                }
+                int color = getColor();
+                if (color != -1) { // -1 means no colour or unrecognised colour
+                    colorList[colorCount++] = color;
+                    beepOnce();
+                }
+                Delay.msDelay(1000);
+            }
+
+            // Sorting loop
+            for (int i = 0; i < colorCount; i++) {
+                sortColor(colorList[i]);
+                ejectObject();
+            }
+            
+            // Move to start position after sorting
+            moveToColorPosition(0);
+
+            // Check for back button press to exit or continue for a new round of scanning and sorting
+            if (Button.ESCAPE.isDown()) {
+                break; // Exit program
+            }
+
+            LCD.clear();
+            // Optional: Add a small delay or a beep here to indicate readiness for next round
+            Delay.msDelay(1000);
+        }
+    }
+
+    private static void resetMotors() {
+        beltMotor.resetTachoCount();
+        feedMotor.resetTachoCount();
+    }
+
+    private static int getColor() {
+        int colorId = colorSensor.getColorID();
+        String colorName = ""; // Initialize color name as an empty string
+        switch (colorId) {
+            case Color.BLACK:
+                colorName = "BLACK";
+                break;
+            case Color.BLUE:
+                colorName = "BLUE";
+                break;
+            case Color.GREEN:
+                colorName = "GREEN";
+                break;
+            case Color.YELLOW:
+                colorName = "YELLOW";
+                break;
+            case Color.RED:
+                colorName = "RED";
+                break;
+            case Color.WHITE:
+                colorName = "WHITE";
+                break;
+            case Color.BROWN:
+                colorName = "BROWN";
+                break;
+            default:
+                colorName = "UNKNOWN";
+                break;
+        }
+        LCD.clear(); // Clear the screen before displaying the new color name
+        LCD.drawString(colorName, 0, 1); // Display the color name on the screen
+        return colorId;
+    }
+
+
+    private static void sortColor(int colorId) {
+        String colorName = getColorName(colorId); // Convert colorId to colorName
+        LCD.clear();
+        LCD.drawString("Sorting: " + colorName, 0, 0);
+        switch (colorId) {
+            case Color.RED:
+                moveToColorPosition(COLOR_POSITIONS[0]);
+                break;
+            case Color.GREEN:
+                moveToColorPosition(COLOR_POSITIONS[1]);
+                break;
+            case Color.BLUE:
+                moveToColorPosition(COLOR_POSITIONS[2]);
+                break;
+            case Color.YELLOW:
+                moveToColorPosition(COLOR_POSITIONS[3]);
+                break;
+            default:
+                LCD.drawString("Unknown Color", 0, 1);
+                break;
+        }
+    }
+
+    private static String getColorName(int colorId) {
+        switch (colorId) {
+            case Color.BLACK:
+                return "BLACK";
+            case Color.BLUE:
+                return "BLUE";
+            case Color.GREEN:
+                return "GREEN";
+            case Color.YELLOW:
+                return "YELLOW";
+            case Color.RED:
+                return "RED";
+            case Color.WHITE:
+                return "WHITE";
+            case Color.BROWN:
+                return "BROWN";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+
+    private static void moveToColorPosition(int position) {
+        beltMotor.rotateTo(position, true);
+        while (beltMotor.isMoving()) {
+            Delay.msDelay(1000);
+        }
+    }
+
+    private static void ejectObject() {
+        feedMotor.rotate(90);
+        Delay.msDelay(500);
+        feedMotor.rotate(-90);
+    }
+
+    private static void beepOnce() {
+        Sound.playTone(1000, 100);
     }
 }
